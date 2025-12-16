f5e041514d7a0f09c6bdc299aa406582b6539243
Mateus Sousa <mateusfj8144@gmail.com>
2025-12-16 15:45:00 -0300

    feat(login): cria rota de login

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,41 @@
+import {
+  ErrorMapperResponse,
+  mapError,
+} from "@/src/backend/shared/errors/error-mapper";
+import {
+  LoginSchema,
+  loginSchema,
+  ResponseLoginSchema,
+} from "@/src/validators/auth.schema";
+import { NextResponse } from "next/server";
+import { IResponse } from "@/src/types/Response/IResponse";
+import { LoginUseCase } from "@/src/backend/application/use-cases/auth/login-usecase/login.usecase";
+
+export async function POST(req: Request): Promise<NextResponse> {
+  try {
+    const body = await req.json();
+
+    const data: LoginSchema = loginSchema.parse(body);
+
+    const loginUseCase = new LoginUseCase();
+
+    const response: IResponse<ResponseLoginSchema> = await loginUseCase.execute(
+      data
+    );
+
+    return NextResponse.json({
+      token: response.data?.token,
+      user: {
+        id: response.data?.id,
+        email: response.data?.email,
+        name: response.data?.name,
+      },
+    });
+  } catch (error: unknown) {
+    const mappedError: ErrorMapperResponse = mapError(error);
+
+    return NextResponse.json(mappedError.body, {
+      status: mappedError.statusCode,
+    });
+  }
+}
