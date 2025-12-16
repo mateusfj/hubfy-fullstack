afa343b2ada934588fd719809dc434dafd81ad65
Mateus Sousa <mateusfj8144@gmail.com>
2025-12-16 13:25:31 -0300

    feat(auth): expor rota POST /api/auth/register

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,36 @@
+import {
+  ErrorMapperResponse,
+  mapError,
+} from "@/src/backend/shared/errors/error-mapper";
+import {
+  RegisterSchema,
+  registerSchema,
+  ResponseRegisterSchema,
+} from "@/src/validators/auth.schema";
+import { NextResponse } from "next/server";
+import { RegisterUserUseCase } from "@/src/backend/application/use-cases/auth/create-user/create-user.usecase";
+import { IResponse } from "@/src/types/Response/IResponse";
+
+export async function POST(req: Request): Promise<NextResponse> {
+  try {
+    const body = await req.json();
+
+    const data: RegisterSchema = registerSchema.parse(body);
+
+    const registerUserUseCase = new RegisterUserUseCase();
+
+    const response: IResponse<ResponseRegisterSchema> =
+      await registerUserUseCase.execute(data);
+
+    return NextResponse.json({
+      message: "Usuário criado com sucesso",
+      user: response.data,
+    });
+  } catch (error: unknown) {
+    const mappedError: ErrorMapperResponse = mapError(error);
+
+    return NextResponse.json(mappedError.body, {
+      status: mappedError.statusCode,
+    });
+  }
+}
